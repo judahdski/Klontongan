42982ea383ef1bc219fe5e5761944202012bb2b7
judahdski <howtomister7@gmail.com>
2021-04-23 21:05:35 +0700

    latihan js ttg popup box

---
+++ .latihan/src/latihanjs/script.js
@@ -0,0 +1,10 @@
+alert('halo selamat datang');
+var test = true;
+
+while (test == true) {
+    var name = prompt('Siapa namamu?');
+    alert(name);
+    test = confirm('mau coba lagi gaa?');
+}
+
+alert('terimakasih');
--- latihan/src/latihanjs/script.js
+++
@@ -1,3 +0,0 @@
-var x = 10;
-console.log('Heelloooo Judah');
-console.log('Isi dari variabel x adalah ' + x);
