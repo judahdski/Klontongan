b32b6df40b7d0727e63f351c066ca9e95cb3eaeb
judahdski <howtomister7@gmail.com>
2021-04-24 16:05:40 +0700

    latihan perulangan dan pengkondisian

--- .latihan/src/latihanjs/script.js
+++ .latihan/src/latihanjs/script.js
@@ -1,11 +1,25 @@
 alert('halo selamat datang');
-var test = true;
+var jumlahAngkot = 10;
 
-while (test == true) {
-    var name = prompt('Siapa namamu?');
-    alert(name);
-    test = confirm('mau coba lagi gaa?');
-    // karna confirm itu balikin nilai boolean
+for (var noAngkot = 1; noAngkot <= jumlahAngkot; noAngkot++) {
+    var pernyataan = 'Angkot No. ' + noAngkot;
+    if (noAngkot <= 6) {
+        console.log(pernyataan + ' beroperasi dengan baik');
+    } else if (noAngkot === 8) {
+        console.log(pernyataan + ' sedang lembur.');
+    } else {
+        console.log(pernyataan + ' tidak beroperasi dengan baik.');
+    }
+}
+
+var angka = prompt('Masukan angka yang anda suka :');
+
+if (angka % 2 === 0) {
+    console.log(angka + ' merupakan bilangan genap');
+} else if (angka % 2 === 1) {
+    console.log(angka + ' merupakan bilangan ganjil');
+} else {
+    console.log('Input anda bukan merupakan sebuah angka!');
 }
 
 alert('terimakasih');
