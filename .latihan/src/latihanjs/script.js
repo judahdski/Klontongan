a129bc57860ceba83ffebea7cbc871a8249375b5
judahdski <howtomister7@gmail.com>
2021-04-26 16:35:22 +0700

    belajar javascript ttg array

--- .latihan/src/latihanjs/script.js
+++ .latihan/src/latihanjs/script.js
@@ -1,25 +1,14 @@
-alert('halo selamat datang');
-var jumlahAngkot = 10;
+var shoppingList = [
+    ['Chocolate Bar', 16],
+    ['Silver Quinn', 15],
+    ['Citato', 17],
+];
 
-for (var noAngkot = 1; noAngkot <= jumlahAngkot; noAngkot++) {
-    var pernyataan = 'Angkot No. ' + noAngkot;
-    if (noAngkot <= 6 && noAngkot !== 5) {
-        console.log(pernyataan + ' beroperasi dengan baik');
-    } else if (noAngkot === 8 || noAngkot === 10 || noAngkot === 5) {
-        console.log(pernyataan + ' sedang lembur.');
-    } else {
-        console.log(pernyataan + ' tidak beroperasi dengan baik.');
-    }
-}
+var storeData = shoppingList.unshift('Judah');
+console.log(shoppingList);
 
-var angka = prompt('Masukan angka yang anda suka :');
+shoppingList.shift();
+shoppingList.unshift(['Judah', 18]);
 
-if (angka % 2 === 0) {
-    console.log(angka + ' merupakan bilangan genap');
-} else if (angka % 2 === 1) {
-    console.log(angka + ' merupakan bilangan ganjil');
-} else {
-    console.log('Input anda bukan merupakan sebuah angka!');
-}
-
-alert('terimakasih');
+// shift / unshift for change first data of the array
+// push / pop for change last data of the array
