6a371659011e1b572950a30827e174e77baf2a94
judahdski <howtomister7@gmail.com>
2021-05-04 21:01:19 +0700

    toko-page:selesai template box products

--- .latihan/src/latihanjs/script.js
+++ .latihan/src/latihanjs/script.js
@@ -1,3 +1,9 @@
-var test = alert('data berhasil dimasukan!');
+var s = '';
+for (var i = 0; i < 10; i++) {
+    for (var j = 10; j > i; j--) {
+        s += '*';
+    }
+    s += '\n';
+}
 
-console.log(test);
+console.log(s);
