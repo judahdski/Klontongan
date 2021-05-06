300555c64709af032cff96cdfdc2648def19fd50
judahdski <howtomister7@gmail.com>
2021-05-06 13:15:22 +0700

    latihan javascript

--- .latihan/src/latihanjs/script.js
+++ .latihan/src/latihanjs/script.js
@@ -1,9 +1,38 @@
-var s = '';
-for (var i = 0; i < 10; i++) {
-    for (var j = 10; j > i; j--) {
-        s += '*';
+let data = [10, 9, 8, 7, 6, 5, 2, 1, 4, 3];
+
+console.log(data);
+console.log('Penghapusan node: ');
+data.shift();
+console.log(data);
+console.log('Leaf akan menempati root: ');
+
+let dataTerakhir = data.pop();
+console.log(dataTerakhir);
+data.unshift(dataTerakhir);
+console.log(data);
+
+let i = data.indexOf(dataTerakhir);
+let anakKiri = 2 * i + 1;
+let anakKanan = 2 * i + 2;
+let siblingKiri = i - 1;
+let siblingKanan = i + 1;
+let parents = (i - 1) / 2;
+console.log(data[anakKiri]);
+console.log(data[anakKanan]);
+
+let temp = data.shift();
+data.unshift(temp);
+
+while (data[i] < data[anakKiri]) {
+    let j = data.indexOf(dataTerakhir);
+    if (data[anakKiri] < data[anakKanan]) {
+        data[i] = data[anakKanan];
+        data[anakKanan] = dataTerakhir;
+    } else {
+        data[i] = data[anakKiri];
+        data[anakKiri] = dataTerakhir;
     }
-    s += '\n';
 }
 
-console.log(s);
+console.log(data);
+console.log(i);
