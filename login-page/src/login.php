c9ee156129e74bd9504efd2f5b94e00ca2cb48f5
vitriaagrn <71357806+vitriaagrn@users.noreply.github.com>
2021-05-12 11:19:46 +0700

    login.php
    php buat login, tapi ada sedikit perubahan di tombol masuk. diubah dari input jadi button

--- login-page/src/login.php
+++ login-page/src/login.php
@@ -6,21 +6,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Silahkan Daftar</title>
-    <link rel="stylesheet" href="css/mainstyle.css">
+    <link rel="stylesheet" href="mainstyle.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link
         href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
         rel="stylesheet">
 </head>
 
+ <?php 
+		require 'connect2.php';
+			if (isset($_POST["login"])){
+				ceklogin($_POST);
+			}
+?>
+
 <body>
     <div class="container">
         <div class="kotak-hitam">
             <h3>Selamat Datang</h3>
-            <form action="signup-form" class="signup-form">
+            <form action="" class="signup-form" method="POST">
                 <input type="email" name="email" id="email">
                 <input type="password" name="password" id="password">
-                <input type="submit" value="Masuk" class="button-submit">
+                <button  type="submit" name="login"><a href="toko.php">Masuk</button>
             </form>
         </div>
     </div>
