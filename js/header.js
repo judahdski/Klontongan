0381cefcb0991c7efff8eafb7964742637b9ab69
judahdski <howtomister7@gmail.com>
2021-06-04 12:20:49 +0700

    buat nav header for desktop

--- js/header.js
+++ js/header.js
@@ -1,12 +1,24 @@
-let hamburger = document.querySelector('.hamburger');
-let navLink = document.querySelector('.nav-link-mobile');
+// let hamburger = document.querySelector('.hamburger');
+// let navLink = document.querySelector('.nav-link-mobile');
 
-hamburger.addEventListener('click', function (event) {
-    navLink.setAttribute('id', 'nav-active');
+// hamburger.addEventListener('click', function (event) {
+//     navLink.setAttribute('id', 'nav-active');
+// });
+
+// let body = document.getElementById('main');
+
+// body.addEventListener('click', function (event) {
+//     navLink.setAttribute('id', '');
+// });
+
+// Nav desktop ~ profile user
+let imageUser = document.querySelector('.picture-user');
+let profileDropdown = document.querySelector('.profile-setting');
+
+imageUser.addEventListener('click', function (event) {
+    profileDropdown.setAttribute('id', '');
 });
 
-let body = document.getElementById('main');
-
-body.addEventListener('click', function (event) {
-    navLink.setAttribute('id', '');
+profileDropdown.addEventListener('click', function (event) {
+    profileDropdown.setAttribute('id', 'profile-setting-non-active');
 });
